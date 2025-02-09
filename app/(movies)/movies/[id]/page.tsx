0a3bd3a2eade92d3a88288d8a7e3558c7d3e808a
kimyoungsoo <qhdl301@gmail.com>
2025-02-09 17:24:55 +0900

    refactor(movies): next 15 부터params, searchParams 사용시 Promise 객체 포함하여 사용하여 코드 변경

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,19 +2,23 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
+type ParamsType = Promise<{ id: string }>;
 
 // 예약어
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata(props: { params: ParamsType }) {
+  const params = await props.params;
+  const id = params.id;
   const movie = await getMovie(id);
+
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage(props: { params: ParamsType }) {
+  const params = await props.params;
+  const id = params.id;
+
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
